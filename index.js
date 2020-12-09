33a6ee9eea2a623602cfe13e373dabc41ee1c7fd
DerrickDarku <derrickalberto@ymail.com>
2020-12-09 09:00:17 +0000

    request and response initiated

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const http = require ('http');
 
 const handleAllRequests = (requestObject, responseObject) => {
     console.log('Hi, I just received a request');
-    
-    const url = responseObject.url;
+    const url = requestObject.url;
     if(url === '/'){
-        responseObject.write("<h1>Welcome  to the home page</h1>");
+        responseObject.write("<h1>Welcome to my homepage</h1>");
     }else if(url === '/login'){
-        responseObject.write("<h2>Welcome to Login page</h2>");
+        responseObject.write("<h2>Welcome to login page</h2>");
     }else if(url === '/signup'){
-        responseObject.write("<h2>Welcometo the sign up page</h2>");
+        responseObject.write("<h2>Welcome to signup page</h2>");
     }else{
-        responseObject.write("<h1>Page is found</h1>");
+        responseObject.write("<h1>Page not found</h1>");
     }
+
     responseObject.end();
 }
 
 const server = http.createServer(handleAllRequests);
 
-server.listen(3000, '127.0.0.1',()=> console.log('server is ready to accept request') );   
+server.listen(3000, '127.0.0.1', ()=> console.log('server is ready to accept request') );   
