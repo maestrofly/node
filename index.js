8aeaf3a665f7f042d16f9fb044137c1f275e4348
DerrickDarku <derrickalberto@ymail.com>
2020-12-15 15:04:35 +0000

    routing

--- index.js
+++ index.js
@@ -4,20 +4,35 @@ const handleAllRequests = (requestObject, responseObject) => {
     console.log('Hi, I just received a request');
     const url = requestObject.url;
     responseObject.setHeader("content-type", "text/html");
-    if(url === '/'){
-        responseObject.write("<h1>Welcome to my homepage</h1>");
-    }else if(url === '/login'){
-        responseObject.write("<h2>Welcome to login page</h2>");
-    }else if(url === '/signup'){
-        responseObject.write("<h2>Welcome to signup page</h2>");
-    }else{
-        responseObject.write("<h1>Page not found</h1>");
-    }
+    
+    responseObject.write("<h1>Page not found</h1>");
+    
 
     responseObject.end();
 }
 
+const handleHomeRequest = (req, res) => {
+    res.send("<h1>Welcome to my homepage</h1>");
+}
+
+const handleLoginRequest = (req, res) => {
+    res.send("<h2>Welcome to login page</h2>");
+}
+
+const handleSignupRequest = (req, res) => {
+    res.send("<h2>Welcome to signup page</h2>");
+}
+
+const handleProfileRequest = (req, res) => {
+    res.send("<h2>Welcome to the profile page</h2>");
+}
+
 const server = express();
+server.use('/', handleHomeRequest);
+server.use('/profile', handleProfileRequest);
+server.use('/login', handleLoginRequest);
+server.use('/signup', handleSignupRequest);
+
 
 server.use(handleAllRequests);
 
