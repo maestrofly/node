32f4f503c27fc28f8f6e5da9e47dc7cf9b4fd533
DerrickDarku <derrickalberto@ymail.com>
2021-02-10 06:40:53 -0700

    route specific middleware

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const express = require ('express');
 
-const handleAllRequests = (requestObject, responseObject) => {
-    console.log('Hi, I just received a request');
-    const url = requestObject.url;
-    responseObject.setHeader("content-type", "text/html");
-    
-    responseObject.write("<h1>Page not found</h1>");
-    
-
-    responseObject.end();
+const server = express();
+const handleAllRequests = (req, res) =>{
+    res.send("RESPONSE FROM SERVER.USE");
 }
+
 
 const handleHomeRequest = (req, res) => {
     res.send("<h1>Welcome to my homepage</h1>");
 }
 
-const handleLoginRequest = (req, res) => {
-    res.send("<h2>Welcome to login page</h2>");
+const handleLoginGetRequest = (req, res) => {
+    res.send('Welcome to login page');
 }
 
 const handleSignupRequest = (req, res) => {
@@ -27,13 +22,83 @@ const handleProfileRequest = (req, res) => {
     res.send("<h2>Welcome to the profile page</h2>");
 }
 
-const server = express();
-server.use('/', handleHomeRequest);
-server.use('/profile', handleProfileRequest);
-server.use('/login', handleLoginRequest);
-server.use('/signup', handleSignupRequest);
+const handleProfilePutRequestType = (req, res)=> {
+    res.send('RESPONDED TO PROFILE REQUEST WITH METHOD OF VALUE PUT');
+}
+
+const middlewarefunction = (req, res, next) =>{
+    // console.log('req');
+    //make some checks
+
+    next();
+   
+}
+
+const loginRouteSpecificMiddleware = (req, res, next) => {
+    console.log('login route middleware executed');
+    next();
+}
+//middlewares
+// server.use(middlewarefunction);
 
 
-server.use(handleAllRequests);
+//routes
+server.post('/profile', handleAllRequests);
+server.get('/login', loginRouteSpecificMiddleware, handleLoginGetRequest);
 
-server.listen(3000, '127.0.0.1', ()=> console.log('server is ready to accept request') );   
+server.put('/profile', handleProfilePutRequestType );
+server.get('/signup', (req, res)=> res.send('FROM SIGNUP FOR GET METHOD'));
+server.post('/signup', (req, res)=> res.send('FROM SIGNUP FOR POST METHOD'));
+server.get('/', (req, res)=> res.send('FROM SIGNUP FOR GET METHOD'));
+
+
+server.listen(3000,() => console.log('MAESTROFLYS SERVER IS READY'));
+
+// server.listen(3000, '127.0.0.1', ()=> console.log('MAESTROFLYS SERVER IS READY') );        
+
+
+
+
+
+
+
+
+
+
+
+
+// const handleAllRequests = (requestObject, responseObject) => {
+//     console.log('Hi, I just received a request');
+//     const url = requestObject.url;
+//     responseObject.setHeader("content-type", "text/html");
+    
+//     responseObject.write("<h1>Page not found</h1>");
+    
+
+//     responseObject.end();
+// }
+
+
+
+// const server = express();
+
+// const middlewarefunction = (req, res, next) => {
+//     console.log('hello this is the middleware');
+//     //make some checks
+//      res.send(req);
+//          next();
+   
+// }
+
+
+
+// server.use(middlewarefunction);
+
+//routes 
+// server.use('/profile', handleProfileRequest);
+
+// server.use('/signup', handleSignupRequest);  
+// server.use('/', handleHomeRequest);
+
+
+// server.use(handleAllRequests);
